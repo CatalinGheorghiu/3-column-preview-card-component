812f9bbde104cd6868b92265069bb3be162c80a4
Catalin Gheorghiu <ionutcatalingheorghiu@yahoo.com>
2021-09-13 11:02:51 +0300

    Styled card

---
+++ README.md
@@ -0,0 +1,46 @@
+# Frontend Mentor - Stats preview card component solution
+
+This is a solution to
+the [Stats preview card component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/stats-preview-card-component-8JqbgoU62)
+. Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+
+### Screenshot
+
+![](./desktop-preview.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [https://frontend-mentor-stats-preview.netlify.app](https://frontend-mentor-stats-preview.netlify.app)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [VueJs](https://vuejs.org/) - JS library
+- [SCSS](https://sass-lang.com/) - For styles
+
+## Author
+
+- Website - [Catalin Gheorghiu](https://github.com/CatalinGheorghiu)
+- Frontend Mentor - [@CatalinGheorghiu](https://www.frontendmentor.io/profile/CatalinGheorghiu)
+
+## Acknowledgments
+
+Special thanks to my mentors:
+
+- **Mihai-Adrian Andrei**
+- **Arpan Chattopadhyay**
+- **Ivan Saldaño**
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,52 @@
 <template>
   <div id='app'>
+    <PreviewCard v-for='car in cars'
+                 :key='car.id'
+                 :background-color='car.cardBgColor'
+                 :description='car.description'
+                 :header-title='car.carType'
+                 :link='car.linkTo'>
+      <template v-slot:headerIcon>
+        <img :alt='`${car.carType} icon`'
+             :src='require(`@/images/icon-${car.carType}.svg`)'>
+      </template>
+    </PreviewCard>
   </div>
 </template>
 
 <script>
+import PreviewCard from './components/PreviewCard.vue';
 
 export default {
   name: 'App',
-  components: {}
+  components: { PreviewCard },
+  data() {
+    return {
+      cars: [
+        {
+          id: 1,
+          carType: 'sedans',
+          description: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
+          linkTo: 'www.sedans.com',
+          cardBgColor: 'orange'
+        },
+        {
+          id: 2,
+          carType: 'suvs',
+          description: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacations and off-road adventures',
+          linkTo: 'www.suvs.com',
+          cardBgColor: 'light-cyan'
+        },
+        {
+          id: 3,
+          carType: 'luxury',
+          description: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
+          linkTo: 'www.luxury.com',
+          cardBgColor: 'dark-cyan'
+        }
+      ]
+    };
+  }
 };
 </script>
 
